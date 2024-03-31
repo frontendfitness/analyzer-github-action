f29d90bae51ce273635f4f873c0a1579d23009b0
Michael Low <mikelow@gmail.com>
2024-03-31 14:31:13 +0100

    chore: updating naming to analyzer-github-action

--- dist/index.js
+++ dist/index.js
@@ -25965,17 +25965,17 @@ const core = __importStar(__nccwpck_require__(2186));
 const exec = __importStar(__nccwpck_require__(1514));
 async function run() {
     try {
-        console.log('tracker index: running the lint script');
+        console.log('Analyzer index: running the lint script');
         await exec.exec('npm run lint');
         // Run ESLint
-        console.log('tracker index: ran ESLint successfully');
+        console.log('Analyzer index: ran ESLint successfully');
     }
     catch (error) {
         console.log('An error occured', error.message);
         core.setFailed(`ESLint check failed: ${error}`);
     }
 }
-console.log('FF quality tracker index');
+console.log('FF quality Analyzer index');
 run();
 
 
--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ import * as exec from '@actions/exec'
 
 async function run() {
   try {
-    console.log('tracker index: running the lint script')
+    console.log('Analyzer index: running the lint script')
     await exec.exec('npm run lint')
 
     // Run ESLint
-    console.log('tracker index: ran ESLint successfully')
+    console.log('Analyzer index: ran ESLint successfully')
   } catch (error) {
     console.log('An error occured', (error as Error).message)
     core.setFailed(`ESLint check failed: ${error}`)
   }
 }
 
-console.log('FF quality tracker index')
+console.log('FF quality Analyzer index')
 
 run()
