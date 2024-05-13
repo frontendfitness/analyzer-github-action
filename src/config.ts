4df5b8d1fb112d137d6175d93cb09cb1e51afb51
Michael Low <mikelow@gmail.com>
2024-05-14 00:03:33 +0100

    update workers domains

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_HOST = 'https://analyzer-api.frontendfitness.workers.dev'
+export const API_HOST = 'https://analyzer-api.incrementalcode.workers.dev'
 export const API_CONFIG_URL = `${API_HOST}/analyzer/config`
